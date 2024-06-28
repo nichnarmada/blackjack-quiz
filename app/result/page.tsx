831a99a6b00e9fa17fdbdbba04b719cf9faf41a0
Nicholas Narmada <nichnarmada@gmail.com>
2024-06-27 22:48:22 -0400

    create results page

---
+++ app/result/page.tsx
@@ -0,0 +1,26 @@
+export default function Quiz() {
+  const correctAnswers = 4
+  const totalQuestions = 5
+
+  return (
+    <div className="prose container mx-auto px-4 py-12 md:px-6 lg:px-8">
+      <div className="max-w-3xl mx-auto">
+        <h1>Quiz Results</h1>
+        <h2>Congratulations for Completing the Quiz!</h2>
+        <p>
+          You've just completed the Blackjack Perfect Strategy Daily Quiz. Let's
+          see how well you did!
+        </p>
+        <h2>Your Score:</h2>
+        <h1 className="text-center">
+          {correctAnswers}/{totalQuestions}
+        </h1>
+        <p>
+          You answered correctly {correctAnswers} out of {totalQuestions}{" "}
+          questions!
+        </p>
+        <p>Accuracy: {(correctAnswers / totalQuestions) * 100}%</p>
+      </div>
+    </div>
+  )
+}
